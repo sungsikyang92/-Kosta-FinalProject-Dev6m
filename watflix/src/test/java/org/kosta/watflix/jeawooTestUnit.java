2d6f127fb8e607e8a0eb54c2b60c1fc1c6299075
772testCode <772tjdwkd@gmail.com>
2020-11-26 15:09:23 +0900

    재우 - '재우TestUnit'에서 'jeawooTestUnit'으로 이름 변경

--- watflix/src/test/java/org/kosta/watflix/jeawooTestUnit.java
+++ watflix/src/test/java/org/kosta/watflix/jeawooTestUnit.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring-model.xml", "file:src/main/webapp/WEB-INF/spring-security.xml"})
-public class 재우TestUnit {
+public class jeawooTestUnit {
 
 	@Test
 	public void test() {
