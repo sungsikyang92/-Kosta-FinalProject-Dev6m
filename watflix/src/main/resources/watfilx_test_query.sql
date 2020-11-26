3e67d0b2a6eb85e950c205cdfc8fe17c5a7dba17
772testCode <772tjdwkd@gmail.com>
2020-11-26 16:07:49 +0900

    재우 - 'watflix_test_query.sql'에서 테이블 삭제 진행

--- watflix/src/main/resources/watfilx_test_query.sql
+++ watflix/src/main/resources/watfilx_test_query.sql
@@ -1,3 +1,21 @@
 delete from genre
 select * from genre
 select * from CONTENTS
+
+-- jaewooTest
+drop table apply;
+drop table party;
+drop table membership;
+drop table faq;
+drop table grade;
+drop table product_order;
+drop table product;
+drop table report;
+drop table report_type;
+drop table notice;
+drop table review;
+drop table comments;
+drop table contents CASCADE CONSTRAINT;
+drop table genre;
+drop table member CASCADE CONSTRAINT;
+drop table acc_status;
