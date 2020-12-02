9ee3d3f94430b4381b32fc3d158629c46ddc5de2
Mr.K <bcgrhio@naver.com>
2020-12-02 14:32:53 +0900

    상훈 형상관리 환경설정1차테스트

--- watflix/src/main/java/org/kosta/watflix/controller/AdminController.java
+++ watflix/src/main/java/org/kosta/watflix/controller/AdminController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
    
    @Resource
-   AdminService adminService;
+   AdminService adminService;//
       
    @RequestMapping("contentsUpdateAdmin.do")
    public String updateContents() {
