d247ef6020361e0af0ae79344b222b890785d52c
Mr.K <bcgrhio@naver.com>
2020-12-02 17:04:21 +0900

    test

--- watflix/src/main/java/org/kosta/watflix/controller/AdminController.java
+++ watflix/src/main/java/org/kosta/watflix/controller/AdminController.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-//
+//test
 public class AdminController {
    
    @Resource
