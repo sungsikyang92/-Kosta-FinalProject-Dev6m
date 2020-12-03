cf30afd8679fc07be482e395e8b3cacb500cfa97
jikang0105 <jikang0105@naver.com>
2020-12-03 09:30:04 +0900

    지훈 - adminNoticeController 백업본 복구

--- watflix/src/main/java/org/kosta/watflix/controller/AdminNoticeContorller.java
+++ watflix/src/main/java/org/kosta/watflix/controller/AdminNoticeContorller.java
@@ -3,8 +3,11 @@ package org.kosta.watflix.controller;
 import javax.annotation.Resource;
 
 import org.kosta.watflix.model.service.NoticeService;
+import org.kosta.watflix.model.vo.NoticeVO;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,13 +16,42 @@ public class AdminNoticeContorller {
 	@Resource
 	NoticeService noticeService;
 	
-	@RequestMapping("getNoticeListForAdmin.do")
-	public String getNoticeListForAdmin(String pageNo, Model model) {
+	@Secured("ROLE_MEMBER")
+	@RequestMapping("getNoticeList.do")
+	public String getNoticeList(String pageNo, Model model) {
 		if(pageNo == null) {
 			model.addAttribute("noticeList", noticeService.sNoticeGetList());			
 		} else {
 			model.addAttribute("noticeList", noticeService.sNoticeGetList(pageNo));
 		}
-		return "admin/noticeListAdmin";
+		return "notice/noticeList";
 	}
+	
+	@RequestMapping("noticeDetail.do")
+	public String noticeDetail(int noticeNo, Model model) {
+		model.addAttribute("noticeDetail", noticeService.sNoticeGetDetailNoHits(noticeNo));
+		// 조회수 증가 관련 기능 추가 필요
+		return "notice/noticeDetail";
+	}
+	
+	@RequestMapping("noticeWriteForm.do")
+	public String noticeWrite(Model model){
+		return "notice/noticeWriteFrom";
+	}
+	
+	@RequestMapping("noticeUpdateForm.do")
+	public String noticeUpdateForm(int noticeNo, Model model) {
+		model.addAttribute("noticeUpdateForm", noticeService.sNoticeGetDetailNoHits(noticeNo)); 
+		return "notice/noticeUpdateForm";
+	}
+	//@Secured("ROLE_MEMBER")
+		@PostMapping("noticeUpdate.do")
+		public String noticeUpdate(NoticeVO noticeVO, int noticeNo, Model model) {
+			System.out.println(noticeVO);
+			System.out.println(noticeNo);
+			noticeService.sNoticeUpdate(noticeVO);
+			model.addAttribute("noticeDetail", noticeService.sNoticeGetDetailNoHits(noticeNo));
+			// 조회수 증가 관련 기능 추가 필요
+			return "notice/noticeDetail";
+		}
 }
