6c1f4f3d5079d012f71fd0b9a81cbd782d2279a0
yulhwi6 <yulhwi6@gmail.com>
2020-12-04 15:39:51 +0900

    율 - faq tiles 적용

--- watflix/src/main/java/org/kosta/watflix/controller/FaqController.java
+++ watflix/src/main/java/org/kosta/watflix/controller/FaqController.java
@@ -29,13 +29,13 @@ public class FaqController {
 		}else {
 			model.addAttribute("lvo",faqService.sGetFaqList(pageNo));
 		}
-		return "faq/faq_list";
+		return "faq/faq_list.tiles";
 		
 	}
 	// faq 작성 폼 이동
 	@RequestMapping("faqWriteForm.do")
 	public String faqWriteForm() {
-		return "faq/faq_write_form";
+		return "faq/faq_write_form.tiles";
 	}
 	// faq 작성
 	
@@ -60,7 +60,7 @@ public class FaqController {
 	// faq 상세보기
 	@RequestMapping("faqDetail.do")
 	public ModelAndView faqDetail(int faqNo) {
-		return new ModelAndView("faq/faq_detail","fvo",faqService.sFaqDetail(faqNo));
+		return new ModelAndView("faq/faq_detail.tiles","fvo",faqService.sFaqDetail(faqNo));
 	}
 	
 	// faq 삭제
@@ -73,7 +73,7 @@ public class FaqController {
 	@RequestMapping("faqUpdateForm.do")
 	public String faqUpdateForm(int faqNo, Model model) {
 		model.addAttribute("fvo",faqService.sFaqDetail(faqNo));
-	return "faq/faq_update_form";
+	return "faq/faq_update_form.tiles";
 }
 	//faq 수정
 	@PostMapping("faqUpdate.do")
@@ -81,6 +81,6 @@ public class FaqController {
 		// System.out.println();
 		faqService.sFaqUpdate(faqVO);
 		model.addAttribute("fvo",faqService.sFaqDetail(faqVO.getFaqNo()));
-		return "faq/faq_detail";
+		return "faq/faq_detail.tiles";
 	}
 }
