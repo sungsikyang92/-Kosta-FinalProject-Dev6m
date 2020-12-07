53d66af2f2a48b624d937e2507847a0a54fe4375
Mr.K <bcgrhio@naver.com>
2020-12-07 16:34:53 +0900

    상훈 - 영화카테고리 출력

--- watflix/src/main/java/org/kosta/watflix/controller/ContentsController.java
+++ watflix/src/main/java/org/kosta/watflix/controller/ContentsController.java
@@ -5,6 +5,7 @@ import javax.annotation.Resource;
 import org.kosta.watflix.model.service.ContentsService;
 import org.kosta.watflix.model.vo.ContentsVO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,5 +19,12 @@ public class ContentsController {
 	public ModelAndView ContentsDetail(ContentsVO contentsVO) {
 		System.out.println(contentsVO.getContentsNo());
 		return new ModelAndView("contents/contents_detail.tiles", "contentsVO",contentsService.sFindContentsByNo(contentsVO.getContentsNo()));
+	}
+	
+	@RequestMapping("contentsByMovie.do")
+	public String contentsByMovie(Model model) {
+		model.addAttribute("contentsList",contentsService.sGetContentsSelectForType("영화"));
+		model.addAttribute("genre",contentsService.sGetAllGenreList());
+		return "contents/contentsByMovie.tiles";
 	}
 }
