201ea49b8c6891289716edc890e50a05485464a8
yulhwi6 <yulhwi6@gmail.com>
2020-12-10 17:48:24 +0900

    율 - qnaList 기능 완료 - qnaTotalCount 완료 - qnaListController 완료 - qnaList view 완료 - qnaDetail 수정중

--- watflix/src/main/java/org/kosta/watflix/controller/QnAController.java
+++ watflix/src/main/java/org/kosta/watflix/controller/QnAController.java
@@ -1,8 +1,38 @@
 package org.kosta.watflix.controller;
 
+import javax.annotation.Resource;
+
+import org.kosta.watflix.model.service.QnAService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class QnAController {
-
+	@Resource
+	QnAService qnaService;
+	
+	// qna 리스트 출력
+	@RequestMapping("qnaList.do")
+	public String list(Model model, String pageNo) {
+		if(pageNo==null) {
+			model.addAttribute("lvo",qnaService.sGetQnAList());
+		}else {
+			model.addAttribute("lvo",qnaService.sGetQnAList(pageNo));
+		}
+		return "qna/qna_list.tiles";
+	}
+	// qna 작성 폼 이동
+	@RequestMapping("qnaWriteForm.do")
+	public String qnaWriteForm() {
+		return "qna/qna_write_form.tiles";
+	}
 }
+
+
+
+
+
+
+
+
