77dc1c30dba799c73a6a93431652bbe07dd274ce
772testCode <772tjdwkd@gmail.com>
2020-12-18 19:59:19 +0900

    재우 - InputTestJUnit.java - 신고글 데이터 삽입 추가( 수정할 예정 )

--- watflix/src/test/java/org/kosta/watflix/InputTestJUnit.java
+++ watflix/src/test/java/org/kosta/watflix/InputTestJUnit.java
@@ -25,6 +25,8 @@ import org.kosta.watflix.model.service.ReviewService;
 import org.kosta.watflix.model.vo.CommentsVO;
 import org.kosta.watflix.model.vo.ContentsVO;
 import org.kosta.watflix.model.vo.MemberVO;
+import org.kosta.watflix.model.vo.ReportTypeVO;
+import org.kosta.watflix.model.vo.ReportVO;
 import org.kosta.watflix.model.vo.ReviewVO;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -92,7 +94,7 @@ public class InputTestJUnit {
 		address.add("서울");
 		address.add("부산");
 		address.add("북한");
-		int createCount = 10; //생성할 회원 수
+		int createCount = 60; //생성할 회원 수
 		
 		// 컨텐츠 정보
 		List<ContentsVO> contentsList= contentsService.sGetAllContentsList();
@@ -110,16 +112,31 @@ public class InputTestJUnit {
 		// 리뷰 글 작성 정보
 		ReviewVO reviewVO = new ReviewVO();
 		ArrayList<String> reviewContents = new ArrayList<String>();
-		reviewContents.add("재미 있어요.");
-		reviewContents.add("볼만 합니다.");
-		reviewContents.add("들렸다 가요~~~.");
+		reviewContents.add("재미 있으니까 꼭 보세요. 리뷰 끝!");
+		reviewContents.add("들어 오는 건 막지 않지만 나가는 건 다르지.");
+		reviewContents.add("당신은 낚이셨습니다.");
 		reviewContents.add("이제 마지막이 다가오고 있습니다. 모두 힘내세요!");
 		ArrayList<String> reviewTitle = new ArrayList<String>();
-		reviewTitle.add("추천합니다.");
-		reviewTitle.add("리뷰는 내 글이 가장 깔끔!");
+		reviewTitle.add(" 추천합니다.");
+		reviewTitle.add(" 리뷰는 내 글이 가장 깔끔!");
 		reviewTitle.add("에 대해 객관적으로 작성함.");
-				
-		// 회원 생성후  평점,리뷰 작성 반복
+		
+		// 신고글 작성 정보
+		ReportVO reportWrite = new ReportVO();
+		ArrayList<CommentsVO> reportComments = new ArrayList<CommentsVO>();
+		ArrayList<ReviewVO> reportReview = new ArrayList<ReviewVO>();
+		ReportTypeVO reportTypeVO = new ReportTypeVO();
+		ArrayList<Integer> reportTypeNo = new ArrayList<Integer>();
+		reportTypeNo.add(1);
+		reportTypeNo.add(2);
+		ArrayList<String> reportContents = new ArrayList<String>();
+		reportContents.add("욕설 신고합니다.");
+		reportContents.add("음란한 내용 삭제 해주세요.");
+		reportContents.add("욕설 제제 하시는 거죠?");
+		reportContents.add("음란사이트 경로 신고합니다.");
+		int reportCount = 0;
+		
+		// 회원 생성후  평점,리뷰 작성 반복 진행
 		for (int user=0; user < createCount; user++) {
 			// 회원 생성
 			memberVO.setId(id+user);
@@ -132,9 +149,24 @@ public class InputTestJUnit {
 			memberVO.setAddress(address.get(user%address.size()));
 			memberService.sMemberRegister(memberVO);
 			
+			// 신고 작성
+			if(user != 0) {
+				reportWrite.setMemberVO(memberVO);
+				for(int report = 0; report < 30; report++) {
+					reportTypeVO.setReportTypeNo(reportTypeNo.get(reportCount%reportTypeNo.size()));
+					reportWrite.setReportTypeVO(reportTypeVO);
+					reportWrite.setReportContents(reportContents.get(reportCount%reportContents.size()));
+					reportWrite.setReviewVO(reportReview.get(reportCount));
+					reportWrite.setCommentsVO(reportComments.get(reportCount));
+					reportService.sReportWriteComments(reportWrite);
+					reportService.sReportWriteReview(reportWrite);
+					reportCount++;
+				}
+			}
+			
 			for (int i=0; i < contentsList.size(); i++) {
 				contentsNo =contentsList.get(i).getContentsNo();
-				
+								
 				// 평점 작성
 				memberVO.setPoint(memberVO.getPoint()+10);
 				commentsVO.setMemberVO(memberVO);
@@ -146,19 +178,24 @@ public class InputTestJUnit {
 				commentsService.sCommentsWrite(commentsVO);
 				// 평균 별점을 입력한다.
 				contentsService.sUpdateAvgStar((float)commentsService.sSumCommentsStars(contentsNo)/(float)commentsService.sCommentsGetTotalPostCountByContentNo(contentsNo), contentsNo);
+				reportComments.add(commentsVO);
 				
 				// 리뷰 작성
 				memberVO.setPoint(memberVO.getPoint()+15);
 				reviewVO.setMemberVO(memberVO);
 				reviewVO.setReviewContents(reviewContents.get((i+user)%reviewContents.size()));
-				reviewVO.setReviewTitle(reviewTitle.get((i+user)%reviewTitle.size()));
+				reviewVO.setReviewTitle(contentsList.get(i).getContentsTitle()+reviewTitle.get((i+user)%reviewTitle.size()));
 				contentsVO.setContentsNo(contentsNo);
 				reviewVO.setContentsVO(contentsVO);
 				//리뷰 작성
 				reviewService.sReviewWrite(reviewVO);
+				reportReview.add(reviewVO);
+				
 			}
 		}
-		//*/		
+		//*/
+		
+		
 		
 	}
 }
